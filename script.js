c37cec9abe7240490811b0578ba6a02c59110f2e
tsurumi <66055760+gambay@users.noreply.github.com>
2023-12-13 21:58:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let line = document.getElementsByClassName("line")[0]
 let span = document.getElementsByTagName("span")[0]
 let slides = document.getElementsByClassName("feedText")
 let counter = 0
+let blocker = document.getElementsByClassName("buttons")[0]
 let comments = [
     {
         text: "“On the Windows talking painted pasture yet its express parties use. Sure last upon he same as knew next. Of believed or diverted no.”",
@@ -14,19 +15,20 @@ let comments = [
         location: "Usa",
     },
     {
-        text: "“Daniil delay dz”",
-        author: "Kirill Yerenkoff",
+        text: "“подпищики красавцы”",
+        author: "админ",
         location: "Russia",
     },
     {
-        text: "“ya ne mogu pochinit piton”",
-        author: "Daniil Gribachev",
+        text: "“аня лох”",
+        author: "админ",
         location: "Russia",
     },
 ]
 console.log(comments[0].location);
 line.style.width = span.innerHTML
 function feedChange(direction) {
+    blocker.style.pointerEvents = "none"
     if (direction == 'up') {
         counter++
         // document.getElementById("secondSlide").style.zIndex = "999"
@@ -47,21 +49,33 @@ function feedChange(direction) {
                 setTimeout(() => {
                     document.getElementById("secondSlide").style.transition = "0.3s"
                     document.getElementById("secondSlide").style.opacity = 1
+                    blocker.style.pointerEvents = "auto"
                     document.getElementById("firstSlide").style.transition = "0.3s"
                 }, 300);
             }, 300);
         }, 300);
     }
     else {
+        counter--
+        if (counter == -1) {
+            counter = 3
+        }
         document.getElementById("firstSlide").style.transform = "translate(34px, 40px)"
         setTimeout(() => {
+            for (let index = 0; index < slides.length; index++) {
+                slides[index].children[2].innerHTML = comments[(counter + index) % comments.length].author
+                slides[index].children[1].innerHTML = comments[(counter + index) % comments.length].text
+                slides[index].children[3].innerHTML = comments[(counter + index) % comments.length].location
+            }
             document.getElementById("firstSlide").style.transition = "0s"
             document.getElementById("firstSlide").style.opacity = 0
             document.getElementById("firstSlide").style.transform = "translate(0px, 0px)"
             setTimeout(() => {
                 document.getElementById("firstSlide").style.transition = "0.3s"
                 document.getElementById("firstSlide").style.opacity = 1
+                blocker.style.pointerEvents = "auto"
             } , 300)
         }, 300)
     }
 }
+``
